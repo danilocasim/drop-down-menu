042bdbf150a1aa9f1376b123f51e456abf86b859
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-02-12 14:00:52 +0800

    Add a burger navigation dropdown an
    The logic consists of two function toggleVisibility and dropdown to use it invoke the dropDown() with param of burgerclass and dropdowncontent class

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
 import style from "../css/style.css";
+
+function toggleVisibility(dropDownContentClass) {
+  const dropDownContent = document.querySelector(dropDownContentClass);
+
+  if (!dropDownContent.classList.contains("visible")) {
+    dropDownContent.classList.add("visible");
+  } else {
+    dropDownContent.classList.remove("visible");
+  }
+}
+
+function dropDown(burgerClass, dropDownContentClass) {
+  const burger = document.querySelector(burgerClass);
+
+  burger.addEventListener("click", () => {
+    toggleVisibility(dropDownContentClass);
+  });
+}
+
+dropDown(".burger", ".drop-down-content");
